ef459c3fa2fdb6df4b5fc8cb7623a0cec7c06388
Akshanshu Pal <akshanshupal@gmail.com>
2024-08-14 12:38:56 +0000

    Update file local.js

---
+++ config/local.js
@@ -0,0 +1,37 @@
+module.exports = {
+    // Any configuration settings may be overridden below, whether it's built-in Sails
+    // options or custom configuration specifically for your app (e.g. Stripe, Sendgrid, etc.)
+    port: 1333,
+    datastores: {
+        mongodbA: {
+          adapter: 'sails-mongo',
+          url: 'mongodb://localhost:27017/random',        
+        },
+    },
+    models: {
+        datastore: "mongodbA",
+        migrate: "safe",
+    },
+    jwtSettings: {
+        secret: "4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTbNg7jO41EAtl20J5F7Trtwe7OM",
+        refreshSecret: "4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTbNg7jO41EAtl20J5F7Txprw7OM",
+        algorithm: "HS256",
+        refreshRxpiresIn: '180d', //30 days='30d'
+        expiresIn: '1d',//15 mins='15m'
+        issuer: "TRAVEL",
+        audience: "tripbliss.com",
+    },
+    bunnyCDN : {
+      HOSTNAME : 'sg.storage.bunnycdn.com',
+      BASE_URL: 'https://storage.bunnycdn.com',
+      STORAGE_ZONE_NAME : 'travelimg',
+      PASSWORD : '49faa86c-ff53-471b-86bb699c3e44-8aa9-433b',
+      baseUrl: "https://travelimg.b-cdn.net"
+    },
+    redis: {
+        // url:'redis://:edukit339master@216.48.178.79:6379'
+        url: "redis://127.0.0.1:6379",
+        expire: 6*30*24*60*60,//6 months expiry
+    },
+    cdnUrl: "http://localhost:1337",
+  };
