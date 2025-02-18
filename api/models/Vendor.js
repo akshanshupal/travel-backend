60ac05d82987f3a7a437974f77c894bb3e6f7d18
asmita <asmu66614@gmail.com>
2025-02-18 12:36:41 +0530

    vendor api change

--- api/models/Vendor.js
+++ api/models/Vendor.js
@@ -15,6 +15,12 @@ module.exports = {
         company: {
             model: 'company'
         },
+        mobile: {
+            type: 'string'
+        },
+        vendorLocation: {
+            type: 'string'
+        },
         isDeleted: { type: 'boolean'},
         deletedAt: { type: 'ref', columnType: 'datetime' },
         deletedBy: { model: 'user' }
