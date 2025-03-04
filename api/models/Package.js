606c6dfde69dab0c777efa0e7138e6b75408ab0f
asmita <asmu66614@gmail.com>
2025-03-04 17:04:08 +0530

    add feature image key in package

--- api/models/Package.js
+++ api/models/Package.js
@@ -45,6 +45,9 @@ module.exports = {
         url :{
             type: 'string'
         },
+        featureImg: {
+            type: 'string'
+        },
         // mobile: {
         //     type: 'string',
         // },
