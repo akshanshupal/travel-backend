9fd16d2ca7dd28c64aad4b7421b647b503310305
akshanshupal <akshanshupal@gmail.com>
2026-04-22 12:49:37 +0530

    set default issue fix

--- list_companies.js
+++
@@ -1,9 +0,0 @@
-const sails = require('sails');
-sails.load({ environment: 'development' }, function(err) {
-  if (err) return console.error(err);
-  sails.models.companyconfig.find({}).exec(function(err, configs) {
-    if (err) console.error(err);
-    else configs.forEach(c => console.log(c.websiteUrls));
-    sails.lower();
-  });
-});
--- list_companies2.js
+++
@@ -1,9 +0,0 @@
-const sails = require('sails');
-sails.load({ environment: 'development' }, function(err) {
-  if (err) return console.error(err);
-  sails.models.company.find({}).exec(function(err, companies) {
-    if (err) console.error(err);
-    else companies.forEach(c => console.log(c.name, c.id));
-    sails.lower();
-  });
-});
